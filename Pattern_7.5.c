7ae5e7c12f7c17c205589d88af863de66541c27f
prashanth264 <147487774+prashanth264@users.noreply.github.com>
2024-01-04 18:29:00 +0530

    #include<stdio.h> int main() {     int n,i,j,k,a,b;     scanf("%d",&n);     a=n;     for(i=1;i<=n;i++)     {         for(k=1;k<i;k++)         {             printf(" ");         }         for(j=a;j>=1;j--)         {             printf("%c ",64+i);         }         a--;         printf(" ");     } }

---
+++ Pattern_7.5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,a,b;
+    scanf("%d",&n);
+    a=n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<i;k++)
+        {
+            printf(" ");
+        }
+        for(j=a;j>=1;j--)
+        {
+            printf("%c ",64+i);
+        }
+        a--;
+        printf("
+");
+    }
+}
